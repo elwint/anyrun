0c05fb51e1d16a20c3cb4b4d3575707db12591bc
Elwin Tamminga <elwintamminga@gmail.com>
2024-08-18 23:32:49 +0200

    [plugins/rink] Add prefix config

--- plugins/rink/README.md
+++ plugins/rink/README.md
@@ -5,3 +5,12 @@ A simple calculator plugin powered by [Rink](https://github.com/tiffany352/rink-
 ## Usage
 
 Just type in your calculations/unit conversions.
+
+## Configuration
+
+```ron
+// <Anyrun config dir>/rink.ron
+Config(
+  prefix: "",
+)
+```
--- plugins/rink/src/lib.rs
+++ plugins/rink/src/lib.rs
@@ -1,9 +1,30 @@
+use std::fs;
+
 use abi_stable::std_types::{ROption, RString, RVec};
 use anyrun_plugin::*;
 use rink_core::{ast, date, gnu_units, CURRENCY_FILE};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    prefix: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            prefix: "".to_string(),
+        }
+    }
+}
+
+struct State {
+    config: Config,
+    ctx: rink_core::Context,
+}
 
 #[init]
-fn init(_config_dir: RString) -> rink_core::Context {
+fn init(config_dir: RString) -> State {
     let mut ctx = rink_core::Context::new();
 
     let units = gnu_units::parse_str(rink_core::DEFAULT_FILE.unwrap());
@@ -29,7 +50,13 @@ fn init(_config_dir: RString) -> rink_core::Context {
     });
     ctx.load_dates(dates);
 
-    ctx
+    State {
+        config: match fs::read_to_string(format!("{}/rink.ron", config_dir)) {
+            Ok(content) => ron::from_str(&content).unwrap_or_default(),
+            Err(_) => Config::default(),
+        },
+        ctx: ctx,
+    }
 }
 
 #[info]
@@ -41,8 +68,14 @@ fn info() -> PluginInfo {
 }
 
 #[get_matches]
-fn get_matches(input: RString, ctx: &mut rink_core::Context) -> RVec<Match> {
-    match rink_core::one_line(ctx, &input) {
+fn get_matches(input: RString, state: &mut State) -> RVec<Match> {
+    let input = if let Some(input) = input.strip_prefix(&state.config.prefix) {
+        input.trim()
+    } else {
+        return RVec::new();
+    };
+
+    match rink_core::one_line(&mut state.ctx, &input) {
         Ok(result) => {
             let (title, desc) = parse_result(result);
             vec![Match {
